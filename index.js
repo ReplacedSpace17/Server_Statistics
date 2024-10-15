454783d2800b5d895d9be3f8087c298522d2580b
ReplacedSpace17 <replacedspace17@gmail.com>
2024-10-14 19:02:40 -0600

    ds

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const socketIo = require('socket.io');
 const os = require('os-utils');
 const si = require('systeminformation');
 const cors = require('cors');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
 class Server {
   constructor() {
@@ -25,10 +28,10 @@ class Server {
     const diskInfo = await this.getDiskInfo();
     this.diskFree = diskInfo.free;
     this.diskTotal = diskInfo.total;
-    this.diskUsed = diskInfo.used; // Agrega el espacio utilizado
+    this.diskUsed = diskInfo.used; 
     this.networkSpeed = await this.getNetworkSpeed();
-    this.gpuInfo = await this.getGpuInfo(); // Agregamos estadísticas de GPU
-    this.topProcesses = await this.getTopProcesses(); // Obtener procesos con mayor uso de CPU
+    this.gpuInfo = await this.getGpuInfo(); 
+    this.topProcesses = await this.getTopProcesses(); 
   }
 
   getCpuUsage() {
@@ -41,18 +44,18 @@ class Server {
 
   async getDiskInfo() {
     const data = await si.fsSize();
-    const disk = data[1]; // Cambia el índice si es necesario
-    const free = disk.available / (1024 * 1024 * 1024); // Convertir a GB
-    const total = disk.size / (1024 * 1024 * 1024); // Convertir a GB
-    const used = disk.used / (1024 * 1024 * 1024); // Convertir a GB
-    return { free, total, used }; // Retornar el espacio utilizado también
+    const disk = data[1];
+    const free = disk.available / (1024 * 1024 * 1024); 
+    const total = disk.size / (1024 * 1024 * 1024); 
+    const used = disk.used / (1024 * 1024 * 1024); 
+    return { free, total, used };
   }
 
   async getNetworkSpeed() {
     const networkStats = await si.networkStats();
     return {
-      rx: networkStats[0].rx_sec,  // Bytes recibidos por segundo
-      tx: networkStats[0].tx_sec   // Bytes transmitidos por segundo
+      rx: networkStats[0].rx_sec,
+      tx: networkStats[0].tx_sec
     };
   }
 
@@ -74,73 +77,109 @@ class Server {
       .map(proc => ({
         name: proc.name,
         pid: proc.pid,
-        cpu: proc.cpu.toFixed(2), // Porcentaje de uso de CPU
-        mem: (proc.memRss / (1024 * 1024)).toFixed(2) // Memoria utilizada en MB
+        cpu: proc.cpu.toFixed(2), 
+        mem: (proc.memRss / (1024 * 1024)).toFixed(2)
       }));
   }
 }
 
-// Inicializar el servidor Express y Socket.IO
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
-
 
 const serverStats = new Server();
 
-app.use(express.static('public')); // Para servir archivos estáticos
-app.use(cors({
-    origin: '*',  // Permitir todos los orígenes
-    methods: ['GET', 'POST'],
-    credentials: true
+// Middleware para sesiones
+app.use(session({
+  secret: 'mySecretKey', // Clave para firmar la sesión
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 300000 } // Sesión de 5 minutos (300,000 ms)
 }));
 
+// Middleware para parsear body de requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Configuración de encabezados CSP
+app.use(expressCspHeader({ 
+  policies: { 
+      'default-src': [expressCspHeader.NONE], 
+      'img-src': [expressCspHeader.SELF], 
+      'script-src': [expressCspHeader.SELF],
+      'style-src': [expressCspHeader.SELF],
+      'object-src': [expressCspHeader.NONE],
+      'frame-src': [expressCspHeader.NONE],
+      'base-uri': [expressCspHeader.NONE],
+      'form-action': [expressCspHeader.NONE],
+      'frame-ancestors': [expressCspHeader.NONE],
+      'manifest-src': [expressCspHeader.NONE],
+      'media-src': [expressCspHeader.NONE],
+      'worker-src': [expressCspHeader.NONE]
+  } 
+}));
+
+// Configuración de CORS
+const corsOptions = {
+  origin: ['https://segucom.mx', 'http://localhost:3001', 'http://localhost:5500', 'http://127.0.0.1:5500', '*', 'http://192.168.1.68/', 'https://localhost:3000',
+    'https://:192.168.1.90/', 'https://segubackend.com:3000', 'https://segubackend.com'],
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 io.on('connection', (socket) => {
-    console.log('Un cliente se ha conectado');
-    socket.on('disconnect', () => {
-        console.log('Un cliente se ha desconectado');
-    });
+  console.log('Un cliente se ha conectado');
+  socket.on('disconnect', () => {
+    console.log('Un cliente se ha desconectado');
+  });
 });
 
+// Página de login
+app.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/stats');
+  } else {
+    res.sendFile(__dirname + '/public/login.html');
+  }
+});
 
-// Endpoint para servir la página web
-app.get('/server1/stats', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+// Endpoint para manejar login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Credenciales estáticas, cámbialas según sea necesario
+  const validUsername = 'rs17';
+  const validPassword = 'Javier117';
+
+  if (username === validUsername && password === validPassword) {
+    req.session.loggedIn = true;
+    res.redirect('/stats');
+  } else {
+    res.send('Credenciales incorrectas. <a href="/">Inténtalo de nuevo</a>');
+  }
+});
+
+// Endpoint protegido que muestra las estadísticas
+app.get('/stats', (req, res) => {
+  if (req.session.loggedIn) {
+    res.sendFile(__dirname + '/public/index.html');
+  } else {
+    res.redirect('/');
+  }
 });
 
 // Enviar estadísticas al cliente cada 2 segundos
 setInterval(async () => {
   await serverStats.updateStats();
-  
-  // Emitir las estadísticas al cliente
   io.emit('serverStats', serverStats);
-  
-  // Mostrar estadísticas en la consola
-  /*
-  console.log('--- Estadísticas del Servidor ---');
-  console.log(`Uso de CPU: ${serverStats.cpuUsage * 100}%`);
-  console.log(`Memoria Libre: ${serverStats.freeMem} MB`);
-  console.log(`Memoria Total: ${serverStats.totalMem} MB`);
-  console.log(`Espacio en Disco Libre: ${serverStats.diskFree} GB`);
-  console.log(`Espacio en Disco Total: ${serverStats.diskTotal} GB`);
-  console.log(`Espacio en Disco Usado: ${serverStats.diskUsed} GB`);
-  console.log(`Velocidad de Red (Rx): ${serverStats.networkSpeed.rx} Bytes/s`);
-  console.log(`Velocidad de Red (Tx): ${serverStats.networkSpeed.tx} Bytes/s`);
-  console.log('Información de GPU:', serverStats.gpuInfo);
-  console.log('Top 5 Procesos:', serverStats.topProcesses);
-  console.log('----------------------------------');
-    */
-}, 2000); // Actualizar cada 2 segundos
+}, 2000);
 
-// Iniciar el servidor
 const PORT = 9000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
